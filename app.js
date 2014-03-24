b0eb63e3fa0a613205ec331bb24cb2d17a4a6912
Glenn Gillen <me@glenngillen.com>
2014-03-23 22:03:58 -0700

    Add rooms for user and command faker.

--- app.js
+++ app.js
@@ -4,29 +4,19 @@ var server = require('http').createServer(app).listen(3000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(client){
-	client.emit('welcome', {greetings:'Welcome to Chat Room!'});
-	client.on('message', broadcastMessage);
 	client.on('user-join', registerUsername);
-
-	function broadcastMessage(msg){
-		client.get('username', function(err, username){
-			if(!username)
-			username = DEFAULT_USERNAME;
-		var broadcastMsg = {
-			msgOwner:username,
-			msgContent:msg
-		};
-		client.broadcast.emit('message', broadcastMsg);
-		});
-	}
+	client.on('stdin', receiveCommand);
+	client.on('stdout', commandResponse);
 
 	function registerUsername(username){
-		client.set('username', username);
-		var systemInfo = {
-			msgOwner:'System',
-			msgContent:'Your current username is ' + username
-		};
-		client.emit('info', systemInfo);
+		if (username == 'user') client.join('user')
+		if (username == 'command') client.join('command')
+	}
+	function receiveCommand(msg) {
+		client.broadcast.to('command').emit('stdin', msg.command);
+	}
+	function commandResponse(msg) {
+		client.broadcast.to('user').emit('stdout', msg.command);
 	}
 });
 app.use(express.static(__dirname + '/public'));
@@ -34,12 +24,3 @@ app.get('/js/socket.io.js', function(req, res) {
 	res.setHeader('Content-Type', 'text/javascript');
 	res.sendfile('node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js');
 });
-app.post('/command', function(req, res) {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({result: 'Hello world\nMulti-line!!'}));
-});
-
-
-//var server = app.listen(3000, function() {
-//	console.log('Listening on port %d', server.address().port);
-//});
